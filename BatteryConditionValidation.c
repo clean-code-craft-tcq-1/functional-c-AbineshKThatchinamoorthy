47327bd0b57a4788b1da8be7d138e08e51eb77be
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-03-03 09:05:56 +0530

    Update BatteryConditionValidation.c

--- BatteryConditionValidation.c
+++ BatteryConditionValidation.c
@@ -26,9 +26,9 @@ static int testBatteryCond_i(float temperature, float soc, float chargeRate);
 static int testBatteryCond_i(float tempVal_f, float socVal_f, float chargeRateVal_f) {
   int validity_i = 0;
   
-  validity_i |= (checkBatteryCond_i(tempVal_f      ,TEMP_VALID_MIN_VAL        ,TEMP_VALID_MAX_VAL       ,0));     /* Store the temperature status in last bit */
-  validity_i |= (checkBatteryCond_i(socVal_f       ,SOC_VALID_MIN_VAL         ,SOC_VALID_MAX_VAL        ,1)) << 1;/* Store the temperature status in last but 1 bit*/
-  validity_i |= (checkBatteryCond_i(chargeRateVal_f,CHARGE_RATE_VALID_MIN_VAL ,CHARGE_RATE_VALID_MAX_VAL,2)) << 2;/* Store the temperature status in last but 2 bits*/
+  validity_i |= (checkBatteryParam_i(tempVal_f      ,TEMP_VALID_MIN_VAL        ,TEMP_VALID_MAX_VAL       ,0));     /* Store the temperature status in last bit */
+  validity_i |= (checkBatteryParam_i(socVal_f       ,SOC_VALID_MIN_VAL         ,SOC_VALID_MAX_VAL        ,1)) << 1;/* Store the temperature status in last but 1 bit*/
+  validity_i |= (checkBatteryParam_i(chargeRateVal_f,CHARGE_RATE_VALID_MIN_VAL ,CHARGE_RATE_VALID_MAX_VAL,2)) << 2;/* Store the temperature status in last but 2 bits*/
   /* All the 3 states are valid */
   validity_i = (ALL_VALID_STATE == validity_i) ? 1 : 0;
   /*Return the validity status */
@@ -36,7 +36,7 @@ static int testBatteryCond_i(float tempVal_f, float socVal_f, float chargeRateVa
 }
 
 /*---------------------------------------------------------------------------*/
-/*     FUNCTION:    checkBatteryCond_i
+/*     FUNCTION:    checkBatteryParam_i
  */
 /*!    \brief       check whether the Battery parameter is within the range
  * 
@@ -47,7 +47,7 @@ static int testBatteryCond_i(float tempVal_f, float socVal_f, float chargeRateVa
  *     \returns     Battery parameter validity status
  *
 *//*------------------------------------------------------------------------*/
-static int checkBatteryCond_i(float batteryParam_f ,float minRange_f, float maxRange_f,int batParamIndex_i)
+static int checkBatteryParam_i(float batteryParam_f ,float minRange_f, float maxRange_f,int batParamIndex_i)
 {
   
   if((batteryParam_f < minRange_f) || (batteryParam_f > maxRange_f))
