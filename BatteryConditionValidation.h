47013178b2877e092a222242e263d2053f6c8a83
AbineshKThatchinamoorthy <77613419+AbineshKThatchinamoorthy@users.noreply.github.com>
2021-03-09 19:30:42 +0530

    Update BatteryConditionValidation.h

--- BatteryConditionValidation.h
+++ BatteryConditionValidation.h
@@ -22,3 +22,24 @@ char batPar[3][10] = {"temp","soc","chargerate"};
 #define CHARGE_RATE_VALID_MAX_VAL         0.8
 
 #define ALL_VALID_STATE                    7
+
+/* Enum for the possible values of Command Ignition status from BCM */
+typedef enum
+{
+    VSUM_STATE_BCM_ING_STS_INIT   = 0X00,
+    VSUM_STATE_BCM_ING_STS_IGN_LK = 0X01,
+    VSUM_STATE_BCM_ING_STS_ACC    = 0X03,
+    VSUM_STATE_BCM_ING_STS_RUN    = 0X04,
+    VSUM_STATE_BCM_ING_STS_START  = 0X05,
+    VSUM_STATE_BCM_ING_STS_SNA    = 0X07,
+}vsum_BcmCmdIngSts_t;
+
+typedef struct {
+    int prevBattCondnParam_i[3];
+}prevBattCondn_t;
+
+typedef struct {
+    int battCondnParam_i[3];
+}battCondn_t;
+
+
